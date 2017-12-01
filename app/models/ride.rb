76dcb2086d89ae89337b1d557559e4d71b29638c
charlie  lee <ctlee9096@gmail.com>
2017-12-01 04:23:45 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,7 @@ class Ride < ActiveRecord::Base
   # write associations here
   belongs_to :user
   belongs_to :attraction
+
+  def self.take_ride
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # write associations here
   has_many :rides
   has_many :attractions, through: :rides
+  has_secure_password
 end
 #http://guides.rubyonrails.org/active_record_migrations.html#changing-columns
