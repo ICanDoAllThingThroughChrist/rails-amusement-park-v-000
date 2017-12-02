13e3cc6dad1d90c5036991ec2c31ea1aa5525575
charlie  lee <ctlee9096@gmail.com>
2017-12-02 21:33:52 +0000

    Done.

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -9,6 +9,7 @@ describe 'Feature Test: User Signup', :type => :feature do
     user_signup
     expect(current_path).to eq('/users/1')
     expect(page).to have_content("Amy Poehler")
+          #binding.pry
     expect(page).to have_content("Mood")
     expect(page).to have_content("happy")
     expect(page).to have_content("15")
