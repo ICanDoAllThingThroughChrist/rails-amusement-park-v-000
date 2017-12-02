9839100be7d7a7ab96c2140344d3964ef78bf4ee
charlie  lee <ctlee9096@gmail.com>
2017-12-02 16:33:46 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,8 +1,10 @@
 class Ride < ActiveRecord::Base
   # write associations here
-  belongs_to :user
   belongs_to :attraction
-
+  belongs_to :user
   def take_ride
+    if attraction.tickets.to_i > user.tickets.to_i
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    end
   end
 end
