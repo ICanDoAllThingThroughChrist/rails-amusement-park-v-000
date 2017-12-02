1aaa4abba278004e3e818e827a1b3e79690d01ed
charlie  lee <ctlee9096@gmail.com>
2017-12-02 20:43:01 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,30 +4,31 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-     if lots_of_tickets && you_are_tall
-       update_status
-     elsif !lots_of_tickets && you_are_tall
-       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-     elsif lots_of_tickets && !you_are_tall
-       "Sorry. You are not tall enough to ride the #{attraction.name}."
-     else
+    if enough_tickets && tall_enough
+      update_status
+    elsif !enough_tickets && tall_enough
+        "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif enough_tickets && !tall_enough
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
        "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-     end
-   end
+    end
+  end
 
-   def lots_of_tickets
-     self.user.tickets >= self.attraction.tickets
-   end
+  def enough_tickets
+      self.user.tickets >= self.attraction.tickets
+  end
 
-   def you_are_tall
-     self.user.height >= self.attraction.min_height
-   end
+  def tall_enough
+      self.user.height >= self.attraction.min_height
+  end
 
   def update_status
     user.tickets = user.tickets.to_i - attraction.tickets.to_i
     user.nausea = user.nausea.to_i + attraction.nausea_rating.to_i
     user.happiness = user.happiness.to_i + attraction.happiness_rating.to_i
     user.save
+        "Thanks for riding the #{self.attraction.name}!"
     #binding.pry
   end
 
