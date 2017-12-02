9d26a89203e810278b012694b83e658e5d7c9a04
charlie  lee <ctlee9096@gmail.com>
2017-12-02 23:46:41 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,28 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    raise params.inspect
   end
 
   def create
     #raise params.inspect
     @user = User.create(user_params)
-    #raise params.inspect
-    log_in(@user)
-    redirect_to user_path(@user)#1
+      if @user && @user.save
+      #raise params.inspect
+      log_in(@user)
+      redirect_to user_path(@user)#1
+      else
+        flash[:alert] = user.errors.full_messages.join(", ")
+        render 'users/new'
+      end
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    redirect_to root_path unless session[:user_id]
+      @user = User.find_by(id: params[:id])
   end
 
+  private
   def user_params
      params.require(:user).permit(:name, :nausea, :happiness, :tickets, :height, :password, :admin )
   end
