a27656ef7bf25b2b6041199359d6a07876809603
charlie  lee <ctlee9096@gmail.com>
2017-12-03 03:00:58 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 get    '/signin',   to: 'sessions#new'#1
 post   '/signin',   to: 'sessions#create'
-delete '/signout',  to: 'sessions#destroy'
+delete '/logout',  to: 'sessions#destroy'
 root 'welcome#home'
 resources :attractions, :users
 end
